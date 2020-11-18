730d5d016ccab316dc1275ef9af9565501588c84
PratoFeito <paulo.vasconcelos@ccc.ufcg.edu.br>
2020-11-18 00:33:18 -0300

    first stable loop with pygame

---
+++ snake.py
@@ -0,0 +1,45 @@
+import pygame, sys
+from pygame.locals import *
+
+# Initialize program
+pygame.init()
+
+# Setting up color objects
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Setup a pixel display with caption
+display_size = 600
+DISPLAY = pygame.display.set_mode((display_size, display_size))
+DISPLAY.fill(GREEN)
+pygame.display.set_caption("Snake")
+
+# Assign FPS a value
+FPS = 30
+FramePerSec = pygame.time.Clock()
+
+
+#describing snake
+tile_size = 20
+initial_tile_size = 6
+initial_position_x = display_size / 2
+initial_position_y = display_size / 2
+ 
+ 
+# Creating Snake shape
+snake_tile = pygame.draw.rect(DISPLAY, RED, (initial_position_x, initial_position_y, tile_size, tile_size))
+ 
+# Beginning Game Loop
+while True:
+    pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    snake_tile
+    pygame.display.update()
+   
+    FramePerSec.tick(FPS)
