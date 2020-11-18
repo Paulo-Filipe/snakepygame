334515dde44bf90559b57f47bde4bc503bd1a112
PratoFeito <paulo.vasconcelos@ccc.ufcg.edu.br>
2020-11-18 01:12:50 -0300

    describing the constituints of each tile in the snake

--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 from drawsnake import *
 
@@ -6,8 +7,8 @@ from drawsnake import *
 pygame.init()
 
 # Setting up color objects
-BLUE  = (0, 0, 255)
-RED   = (255, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -23,22 +24,36 @@ FPS = 30
 FramePerSec = pygame.time.Clock()
 
 
-#describing snake
+# describing snake
 tile_size = 20
-initial_snake_size = 6
-initial_position_x = display_size / 2
-initial_position_y = display_size / 2
- 
+snake_size = 6
+direction = 'LEFT'
+position_x = display_size / 2
+position_y = display_size / 2
+
+snake_body = []
+for i in range(snake_size):
+    snake_body.append( (position_x + i * tile_size, position_y, direction) )
+
+# describing directions
+snake_directions = {
+    'LEFT':  (-tile_size, 0),
+    'RIGHT': (tile_size, 0),
+    'UP':    (0, -tile_size),
+    'DOWN':  (0, tile_size)
+}
+
 # Beginning Game Loop
 while True:
     pygame.display.update()
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     
-    for i in range(initial_snake_size):
-        draw_snake_tile(DISPLAY, RED, initial_position_x+i*tile_size, initial_position_y, tile_size)
+    for i in range(snake_size):
+        (x, y, direction) = snake_body[i]
+        draw_snake_tile(DISPLAY, RED, x, y, tile_size)
     pygame.display.update()
-   
+
     FramePerSec.tick(FPS)
