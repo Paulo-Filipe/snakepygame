2b5c56b8a28f5de52d69502bc00ce989f7eefd85
PratoFeito <paulo.vasconcelos@ccc.ufcg.edu.br>
2020-11-18 01:50:52 -0300

    setting up for controllers

--- snake.py
+++ snake.py
@@ -11,7 +11,7 @@ pygame.init()
 # describing snake
 tile_size = 20
 snake_size = 6
-direction = 'LEFT'
+direction = pygame.K_LEFT
 position_x = display_size / 2
 position_y = display_size / 2
 
@@ -19,10 +19,10 @@ snake = create_snake(tile_size, snake_size, direction, position_x, position_y)
 
 # describing directions
 snake_directions = {
-    'LEFT':  (-tile_size, 0),
-    'RIGHT': (tile_size, 0),
-    'UP':    (0, -tile_size),
-    'DOWN':  (0, tile_size)
+    pygame.K_LEFT:  (-tile_size, 0),
+    pygame.K_RIGHT: (tile_size, 0),
+    pygame.K_UP:    (0, -tile_size),
+    pygame.K_DOWN:  (0, tile_size)
 }
 
 # Beginning Game Loop
@@ -32,7 +32,10 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    
+        elif event.type == pygame.KEYDOWN:
+            if event.key in [ pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT ]:
+                print(snake_directions[event.key])
+
     for i in range(snake_size-1, -1, -1):
         (x, y, direction) = snake[i]
         draw_snake_tile(DISPLAY, RED, x, y, tile_size)
