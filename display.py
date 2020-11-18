06955943f015985a3078b5e058da38d1e9c1d09d
PratoFeito <paulo.vasconcelos@ccc.ufcg.edu.br>
2020-11-18 02:41:44 -0300

    reorganizing colors in assets and making adding some comments

--- display.py
+++ display.py
@@ -7,7 +7,7 @@ from assets import *
 # Setup a pixel display with caption
 display_size = 600
 DISPLAY = pygame.display.set_mode((display_size, display_size))
-DISPLAY.fill(GREEN)
+DISPLAY.fill(colors['GREEN'])
 pygame.display.set_caption("Snake")
 
 # Assign FPS a value
