8aed57317b03bc2c4ff9bed4e98c78c530cc76a7
PratoFeito <paulo.vasconcelos@ccc.ufcg.edu.br>
2020-11-18 04:09:41 -0300

    removing unecessary code

--- snake_services.py
+++ snake_services.py
@@ -2,16 +2,6 @@ import pygame
 import sys
 from controls import *
 
-tile_size = 20
-
-# describing directions
-snake_directions = {
-    controllers['LEFT']:  (-tile_size, 0),
-    controllers['RIGHT']: (tile_size, 0),
-    controllers['UP']:    (0, -tile_size),
-    controllers['DOWN']:  (0, tile_size)
-}
-
 def draw_snake_tile(display, color, x, y, tile_size):
     return pygame.draw.rect(display, color, (x, y, tile_size, tile_size))
 
